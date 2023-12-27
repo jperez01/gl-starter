3f42ab008692802861f2c5a4bf4ede3bf35230ee
JPerez01 <50503292+piolinest123@users.noreply.github.com>
2023-12-27 15:03:26 -0500

    Fixed header files issue

--- src/ui/editor.h
+++ src/ui/editor.h
@@ -6,7 +6,7 @@
 #include "imgui/imgui_impl_opengl3.h"
 #include "imgui/imgui_impl_sdl.h"
 #include "imgui/imgui_stdlib.h"
-#include "ImGuizmo.h"
+#include "ImGuizmo/ImGuizmo.h"
 
 class BaseRenderer;
 
